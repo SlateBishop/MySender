8dba98726d0f4e6d865a3896718c984903a82bef
Despair <p.a.makulin@gmail.com>
2021-11-12 16:42:53 +0300

    Произвел небольшой рефакторинг: уточнил текст логов

--- app/src/main/java/ru/gb/makulin/mysender/SenderService.kt
+++ app/src/main/java/ru/gb/makulin/mysender/SenderService.kt
@@ -3,21 +3,23 @@ package ru.gb.makulin.mysender
 import android.app.IntentService
 import android.content.Intent
 import android.util.Log
+import java.lang.Thread.sleep
 
 const val INTENT_FILTER = "INTENT FILTER"
 const val MSG_EXTRA = "GET MSG EXTRA"
-const val SEND_MSG_EXTRA = "PUT MSG EXTRA"
+const val SENDED_MSG_EXTRA = "SENDED MSG EXTRA"
 
 class SenderService(name: String = "SenderService") : IntentService(name) {
 
 
     override fun onHandleIntent(intent: Intent?) {
-        Log.d("mylogs", intent?.action.toString())
+        sleep(1000)
+        Log.d("mylogs", "sender intent action" + intent?.action.toString())
         intent?.let {
             val message = it.getStringExtra(MSG_EXTRA)
-            Log.d("mylogs", message!!)
+            Log.d("mylogs", "sender message" + message!!)
             val senderIntent = Intent(INTENT_FILTER)
-            senderIntent.putExtra(SEND_MSG_EXTRA, message)
+            senderIntent.putExtra(SENDED_MSG_EXTRA, message)
             sendBroadcast(senderIntent)
         }
     }
