411d6a4d7812176acfdedb16ee4cc3a77e3ebcbc
Despair <p.a.makulin@gmail.com>
2021-11-12 16:03:40 +0300

    Сделал приложение, которое отправляет широковещательные сообщения через сервис.

--- app/src/main/java/ru/gb/makulin/mysender/MainActivity.kt
+++ app/src/main/java/ru/gb/makulin/mysender/MainActivity.kt
@@ -1,11 +1,30 @@
 package ru.gb.makulin.mysender
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.gb.makulin.mysender.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val INTENT_ACTION = "Sender Intent"
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSend.setOnClickListener {
+            var text = binding.editTextSend.text.toString()
+            if (text == "") {
+                text = "Пустое сообщение"
+            }
+            intent = Intent(this, SenderService::class.java)
+            intent.putExtra(MSG_EXTRA,text)
+            intent.action = INTENT_ACTION
+            startService(intent)
+        }
     }
 }
--- app/src/main/java/ru/gb/makulin/mysender/SenderService.kt
+++ app/src/main/java/ru/gb/makulin/mysender/SenderService.kt
@@ -2,19 +2,24 @@ package ru.gb.makulin.mysender
 
 import android.app.IntentService
 import android.content.Intent
+import android.util.Log
+
+const val INTENT_FILTER = "INTENT FILTER"
+const val MSG_EXTRA = "GET MSG EXTRA"
+const val SEND_MSG_EXTRA = "PUT MSG EXTRA"
 
 class SenderService(name: String = "SenderService") : IntentService(name) {
 
-    private val INTENT_FILTER = "INTENT FILTER"
-    private val GET_MSG_EXTRA = "GET MSG EXTRA"
-    private val PUT_MSG_EXTRA = "PUT MSG EXTRA"
+
 
 
     override fun onHandleIntent(intent: Intent?) {
+        Log.d("mylogs", intent?.action.toString())
         intent?.let {
-            val message = it.getStringExtra(GET_MSG_EXTRA)
+            val message = it.getStringExtra(MSG_EXTRA)
+            Log.d("mylogs", message!!)
             val senderIntent = Intent(INTENT_FILTER)
-            senderIntent.putExtra(PUT_MSG_EXTRA,message)
+            senderIntent.putExtra(SEND_MSG_EXTRA,message)
             sendBroadcast(senderIntent)
         }
     }
